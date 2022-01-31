5e4bceef01ff0ef437c420a1a0f467e6548c9a08
Francisco <fran89rbb@hotmail.com>
2022-01-31 20:31:39 +0100

    Añadiendo llave secreta para firmar el token

--- src/main/java/com/franrodriguez/springboot/rest/empleos/app/auth/AuthorizationServerConfig.java
+++ src/main/java/com/franrodriguez/springboot/rest/empleos/app/auth/AuthorizationServerConfig.java
@@ -66,6 +66,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setSigningKey(JwtConfig.LLAVE_SECRETA);
 		return jwtAccessTokenConverter;
 	}
 
